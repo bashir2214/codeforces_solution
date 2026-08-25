f93fc3a82ae1fb5531562e0dce97ae6a95ea89aa
bashir2214 <bashirkathia6@gmail.com>
2026-08-25 23:38:04 +0500

    Add Hit the Lottery [A] from Codeforces

--- 996/A - Hit the Lottery/solution.cpp
+++ 996/A - Hit the Lottery/solution.cpp
@@ -6,7 +6,7 @@ int main()
 int n;
 cin>>n;
 int bills = 0;
-if(n>=100){
+ 
     bills = bills + n/100;
     n = n%100;
     bills = bills + n/20;
@@ -16,31 +16,8 @@ if(n>=100){
     bills = bills + n/5;
     n = n %5;
     bills = bills + n%5; 
-}
-else if(n>=20){
-    bills = bills + n/20;
-    n = n%20;
-    bills = bills + n/10;
-    n = n%10;
-    bills = bills + n/5;
-    n = n %5;
-    bills = bills + n%5; 
-}
-else if(n>=10){
-    bills = bills + n/10;
-    n = n%10;
-    bills = bills + n/5;
-    n = n %5;
-    bills = bills + n%5; 
-}
-else if(n>=5){
-   bills = bills + n/5;
-    n = n %5;
-    bills = bills + n%5; 
-}
-else {
-   bills = bills + n%5; 
-}
+ 
+ 
 cout<<bills;
     return 0;
 }
